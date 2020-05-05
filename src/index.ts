a3cdc31cf4c73d2d6bbcc42c2c397581ae7bd86f
friaca <v.fria@outlook.com>
2020-05-04 22:33:46 -0300

    [add] scraping albums

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import puppeteer from 'puppeteer';
 import question from './inquirer';
 import getSite from './site';
+import { Album } from './types/music';
 
 const DEFAULT_SITE = getSite('azlyrics');
 
@@ -14,11 +15,14 @@ const IDs = Object.freeze({
     name: IDs.artist,
     message: 'Qual o nome do artista?',
     validate: (input: string) => input.length > 0,
-  }).then(answer => answer as string);
+  }).then(answer => answer[IDs.artist]);
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(DEFAULT_SITE.getArtistSearch(artistName));
+  console.log(DEFAULT_SITE.getArtistSearch(artistName));
+  await page.goto(
+    `https://search.azlyrics.com/search.php?q=${artistName.split(' ').join('+')}&w=artists&p=1`
+  );
 
   const foundArtists = await page.evaluate(() =>
     [...document.querySelectorAll('.panel table.table td a')].map(element => ({
@@ -32,7 +36,43 @@ const IDs = Object.freeze({
     type: 'list',
     message: 'Qual desses?',
     choices: foundArtists.map(({ name }) => ({ name })),
-  }).then(answer => foundArtists.find(artist => artist.name === answer)!);
+  }).then(answer => foundArtists.find(artist => artist.name === answer[IDs.artistChoice])!);
 
   await page.goto(chosenArtist.url);
+
+  const artistAlbums = await page.evaluate(() => {
+    /* 
+      Reliable way of getting album list from AZLyrircs as from 04/05/2020
+    */
+
+    const children = [...document.querySelector('#listAlbum')!.children].filter(
+      child => child.nodeName === 'DIV'
+    );
+
+    const albumElements = children.filter(child => child.classList.contains('album'));
+
+    const indexes = albumElements.map(albumTitle => children.indexOf(albumTitle));
+
+    let discography: Album[] = [];
+
+    for (let i = 0; i < indexes.length; i++) {
+      let album: Album = { name: albumElements[i].firstElementChild!.textContent!, songs: [] };
+      for (let j = indexes[i] + 1; j < indexes[i + 1]; j++) {
+        album.songs.push({
+          name: children[j].firstElementChild!.textContent!,
+          url: children[j].firstElementChild?.getAttribute('href')!,
+        });
+      }
+      discography.push(album);
+    }
+
+    for (let k = indexes[indexes.length - 1] + 1; k < children.length; k++) {
+      discography[discography.length - 1].songs.push({
+        name: children[k].firstElementChild!.textContent!,
+        url: children[k].firstElementChild?.getAttribute('href')!,
+      });
+    }
+
+    return discography;
+  });
 })();
