a4da8e9675ad5f6cf326ff7bdb64e20981486a71
friaca <v.fria@outlook.com>
2020-05-04 22:32:48 -0300

    [change] simpler inquirer return/caller deals with return

--- src/inquirer.ts
+++ src/inquirer.ts
@@ -6,18 +6,6 @@ const DEFAULT_PROMPT: DistinctQuestion = {
   message: '',
 };
 
-export default function questionUser(
-  question: DistinctQuestion
-): Promise<string | { [k: string]: string }> {
-  return inquirer.prompt([{ ...DEFAULT_PROMPT, ...question }]).then(dirtyAnswers => {
-    switch (question.type) {
-      case 'input':
-      case 'list':
-        return dirtyAnswers[question.name as string];
-      case 'checkbox':
-        return dirtyAnswers;
-      default:
-        return dirtyAnswers;
-    }
-  });
+export default function questionUser(question: DistinctQuestion): Promise<{ [k: string]: string }> {
+  return inquirer.prompt([{ ...DEFAULT_PROMPT, ...question }]).then(dirtyAnswers => dirtyAnswers);
 }
