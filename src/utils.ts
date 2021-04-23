9b8c662f690f8e90063acc9345efd31a0f377512
friaca <v.fria@outlook.com>
2021-04-23 01:23:17 -0300

    [add] custom fetch with some options

--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,15 @@
+import nodeFetch from 'node-fetch';
+
 export const sanitize = (text: string): string =>
   text.replace(/((?![a-zA-Z0-9 \u00C0-\u017F\/\\]).)/g, '');
+
+export const fetch = async (url: string, options?: RequestInit | undefined): Promise<string> =>
+  await nodeFetch(url, {
+    ...{
+      method: 'GET',
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0'
+      }
+    },
+    ...{ options }
+  }).then(res => res.text())
